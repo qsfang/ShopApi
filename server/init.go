73fafa790db3d1fa2884a9afffaeb4826293ae35
Yusank <yusankurban@gmail.com>
2017-07-18 16:32:58 +0800

    初始化项目 #1

---
+++ server/init.go
@@ -0,0 +1,70 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/07/18        Yusan Kurban
+ */
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/labstack/echo"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	server 		*echo.Echo
+)
+
+func startServer() {
+	server = echo.New()
+
+	server.Start(configuration.address)
+}
+
+func init() {
+	readConfiguration()
+	initMysql()
+	startServer()
+}
+
+func initMysql() {
+	host := configuration.mysqlHost
+	port:= configuration.mysqlHost
+	user := configuration.mysqlUser
+	pass := configuration.mysqlPass
+	db := configuration.mysqlDb
+	maxIdle := configuration.idle
+	maxConn := configuration.conn
+
+
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", user, pass, host, port, db), maxIdle, maxConn)
+	orm.RegisterDataBase(db, "mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", user, pass, host, port, db), maxIdle, maxConn)
+
+}
