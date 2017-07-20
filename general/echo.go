61652c37d708e5ab8aeaecc7309141b58ae8afd5
Yusank <yusankurban@gmail.com>
2017-07-20 15:30:30 +0800

    添加 http 错误处理 #1

--- general/echo.go
+++ general/echo.go
@@ -28,3 +28,40 @@
  */
 
 package general
+
+import (
+	"net/http"
+	"github.com/labstack/echo"
+	"github.com/labstack/gommon/log"
+)
+
+var (
+	code = http.StatusInternalServerError
+	msg  string
+)
+
+func EchoRestfulErrorHandler(err error, c echo.Context) {
+	log.Error(err)
+
+	if resp, ok := err.(*ErrorResp); ok {
+		code = resp.Code
+		msg = resp.Message
+	} else {
+		msg = http.StatusText(code)
+	}
+
+	if !c.Response().Committed {
+		if c.Request().Method == echo.HEAD {
+			err := c.NoContent(code)
+			if err != nil {
+				c.Logger().Error(err)
+			}
+		} else {
+			err := c.JSON(code, NewErrorWithMessage(code, msg))
+
+			if err != nil {
+				c.Logger().Error(err)
+			}
+		}
+	}
+}
