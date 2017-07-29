a9574bb34ad49cf06abf8c21498d0c5ed5d2c0fc
YangZhengtian <yztian1996@gmail.com>
2017-07-29 16:25:27 +0800

    增加前端数据验证函数

--- models/orders.go
+++ models/orders.go
@@ -74,17 +74,26 @@ type OrmOrders struct {
 	Page       uint64    `json:"page" validate:"required,numeric"`
 	PageSize   uint64    `json:"pagesize" validate:"required,numeric"`
 	AddressID  uint64    `json:"addressid" validate:"required,numeric"`
-	ImageID    uint64    `gorm:"column:imageid"json:"imageid"`
+	ImageID    uint64    `json:"imageid"`
 	Remark     string    `json:"remark"`
 	Created    time.Time `json:"created"`
 	Updated    time.Time `json:"updated"`
+}
+
+type CreateOrder struct{
+	AddressID  uint64    `json:"addressid" validate:"required,numeric"`
+	TotalPrice float64   `json:"totalprice" validate:"required,numeric"`
+	Freight    float64   `json:"freight" validate:"required,numeric"`
+	Payment    float64   `json:"payment" validate:"required,numeric"`
+	Remark     string    `json:"remark"`
+	PayWay     uint8     `json:"payway" validate:"required,numeric"`
 }
 
 func (Order) TableName() string {
 	return "order"
 }
 
-func (osp *OrderServiceProvider) CreateOrder(numberID uint64, o OrmOrders) error {
+func (osp *OrderServiceProvider) CreateOrder(numberID uint64, ord CreateOrder) error {
 	var (
 		err error
 		car Cart
@@ -92,12 +101,12 @@ func (osp *OrderServiceProvider) CreateOrder(numberID uint64, o OrmOrders) error
 
 	order := Order{
 		UserID:     numberID,
-		AddressID:  o.AddressID,
-		TotalPrice: o.TotalPrice,
-		Freight:    o.Freight,
-		Remark:     o.Remark,
+		AddressID:  ord.AddressID,
+		TotalPrice: ord.TotalPrice,
+		Freight:    ord.Freight,
+		Remark:     ord.Remark,
 		Status:     general.OrderFinished,
-		PayWay:     o.PayWay,
+		PayWay:     ord.PayWay,
 		Created:    time.Now(),
 		Updated:    time.Now(),
 	}
@@ -130,7 +139,7 @@ func (osp *OrderServiceProvider) CreateOrder(numberID uint64, o OrmOrders) error
 
 func (osp *OrderServiceProvider) GetOrders(userID uint64, status uint8, pageStart, pageEnd uint64) (*[]Order, error) {
 	var (
-		order  Order
+		order Order
 		orders []Order
 	)
 
@@ -171,8 +180,8 @@ func (osp *OrderServiceProvider) GetOrders(userID uint64, status uint8, pageStar
 
 func (osp *OrderServiceProvider) GetOneOrder(UserID uint64, ID uint64) ([]OrmOrders, error) {
 	var (
-		err      error
-		order    Order
+		err error
+		order Order
 		carts    []Cart
 		getOrder []OrmOrders
 	)
